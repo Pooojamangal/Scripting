9f910482a6d07b6d573303531e909640719b9b62
Start <poojaumangal@gmail.com>
2023-10-20 15:42:54 +0530

    Sms File

--- Watermark.py
+++
@@ -1,5 +0,0 @@
-import PyPDF2
-
-template = PyPDF2.PdfFileReader(open('super.pdf','rb'))
-watermark = PyPDF2.PdfFileReader(open('wtr.pdf','rb'))
-output = PyPDF2.PdfFileWriter()
--- mail.py
+++ mail.py
@@ -1,2 +1,19 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+html = Template(Path('index.html').read_text())
+email = EmailMessage()
+email['from'] = 'Andrei Neagoie'
+email['to'] = 'ladybugcode@gmail.com'
+email['subject'] = 'You won 1,000,000 dollars!'
+
+email.set_content(html.substitute({'name': 'TinTin'}), 'html')
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+  smtp.ehlo()
+  smtp.starttls()
+  smtp.login('poojaumangal@gmail.com', 'Pooja8446@')
+  smtp.send_message(email)
+  print('all good boss!')
